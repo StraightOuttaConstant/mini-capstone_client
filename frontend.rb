b6793f7375c259a519da618a56af279ece2a0b48
JackMarx <seriousfools@gmail.com>
2018-02-01 20:47:27 -0600

    update

--- frontend.rb
+++ frontend.rb
@@ -18,6 +18,7 @@ class Frontend
       puts "        [1.2] Sort products by price"
       puts "        [1.3] Sort products by name"
       puts "        [1.4] Sort products by description"
+      puts "        [1.5] Show products by category"
       puts "    [2] See one product"
       puts "    [3] Create a new product"
       puts "    [4] Update a product"
@@ -40,6 +41,26 @@ class Frontend
         products_sort_action("name")
       elsif input_option == "1.4"
         products_sort_action("description")
+      elsif input_option == "1.5"
+        puts 
+        response = Unirest.get("http://localhost:3000/categories")
+        category_hashs = response.body
+        puts "Categories"
+        puts "-" * 40
+        category_hashs.each do |category_hash|
+          puts "- #{category_hash["name"]}"
+        end
+        puts
+
+        print "Enter a category name: "
+        category_name = gets.chomp
+        response = Unirest.get("http://localhost:3000/products?category=#{category_name}")
+        product_hashs = response.body
+
+        product_hashs.each do |product_hash|
+          puts "- #{product_hash["name"]}"
+        end
+
       elsif input_option == "2"
         products_show_action
       elsif input_option == "3"
